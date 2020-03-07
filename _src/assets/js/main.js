2d8ab4fcfa9abc243d27d730f9e9d53aa7f67471
MarinaCouso <marina.couso.martinez@gmail.com>
2020-03-07 20:46:55 +0100

    #15 First working version of the form and card linked by JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,3 +2,25 @@
 
 // Propuesta de Marina
 
+// let formInputName = document.querySelector('.js-form-input-name');
+
+let formInputName = document.querySelector('.js-form-input-name');
+let formInputJob = document.querySelector('.js-form-input-job');
+
+let cardName = document.querySelector('.js-card-name');
+let cardJob = document.querySelector('.js-card-job');
+
+formInputName.addEventListener('keyup', form);
+formInputJob.addEventListener('keyup', form);
+
+// const card = {
+//   name: ''
+// };
+
+function form(event) {
+  if (event.currentTarget.classList.contains('js-form-input-name')) {
+    cardName.innerHTML = formInputName.value;
+  } else if (event.currentTarget.classList.contains('js-form-input-job')) {
+    cardJob.innerHTML = formInputJob.value;
+  }
+}
