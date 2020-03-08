678282757e83c3e1e8b95d1a53ace8aacc7c590f
MarinaCouso <marina.couso.martinez@gmail.com>
2020-03-08 13:56:03 +0100

    #15 All form done but with reply in <a> and not simplified

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,15 +4,27 @@
 
 const formInput = {
   name: document.querySelector('.js-form-input-name'),
-  job: document.querySelector('.js-form-input-job')
+  job: document.querySelector('.js-form-input-job'),
+  email: document.querySelector('.js-form-input-email'),
+  tel: document.querySelector('.js-form-input-tel'),
+  linkedin: document.querySelector('.js-form-input-linkedin'),
+  github: document.querySelector('.js-form-input-github')
 };
 
 formInput.name.addEventListener('keyup', form);
 formInput.job.addEventListener('keyup', form);
+formInput.email.addEventListener('keyup', form);
+formInput.tel.addEventListener('keyup', form);
+formInput.linkedin.addEventListener('keyup', form);
+formInput.github.addEventListener('keyup', form);
 
 const card = {
   name: document.querySelector('.js-card-name'),
-  job: document.querySelector('.js-card-job')
+  job: document.querySelector('.js-card-job'),
+  email: document.querySelector('.js-card-email'),
+  tel: document.querySelector('.js-card-tel'),
+  linkedin: document.querySelector('.js-card-linkedin'),
+  github: document.querySelector('.js-card-github')
 };
 
 function form(event) {
@@ -20,5 +32,13 @@ function form(event) {
     card.name.innerHTML = formInput.name.value;
   } else if (event.currentTarget.classList.contains('js-form-input-job')) {
     card.job.innerHTML = formInput.job.value;
+  } else if (event.currentTarget.classList.contains('js-form-input-email')) {
+    card.email.innerHTML += `<a title='Ir a email de ${formInput.name.value}' href='mailto:${formInput.email.value}'></a>`;
+  } else if (event.currentTarget.classList.contains('js-form-input-tel')) {
+    card.tel.innerHTML += `<a title='Llamar al teléfono de ${formInput.name.value}' href='${formInput.tel.value}'></a>`;
+  } else if (event.currentTarget.classList.contains('js-form-input-linkedin')) {
+    card.linkedin.innerHTML += `<a title='Ir a Linkedin de ${formInput.name.value}' href='${formInput.linkedin.value}'></a>`;
+  } else if (event.currentTarget.classList.contains('js-form-input-github')) {
+    card.github.innerHTML += `<a title='Ir a Github de ${formInput.name.value}' href='${formInput.github.value}'></a>`;
   }
 }
