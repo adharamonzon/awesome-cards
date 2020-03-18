64e4b9e5db66219a9c69ad07d51e8c2c1ae68737
Margui <margui.berjon@gmail.com>
2020-03-18 19:28:56 +0100

    Palletes in one function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -47,33 +47,28 @@ function form(event) {
 // cambiar los colores
 
 let card = document.querySelector('.js-card');
-let themeOne = document.getElementById('green');
-let themeTwo = document.getElementById('red');
-let themeThree = document.getElementById('other');
+let pallete = document.querySelector('.design__form');
 
-themeOne.addEventListener('change', paletteOne);
+function changePallete(ev) {
+  if (ev.target.id === 'green') {
+    card.classList.add('card--theme1');
+    card.classList.remove('card--theme2');
+    card.classList.remove('card--theme3');
 
-function paletteOne() {
-  card.classList.add('card--theme1');
-  card.classList.remove('card--theme2');
-  card.classList.remove('card--theme3');
+  } else if (ev.target.id === 'red') {
+    card.classList.add('card--theme2');
+    card.classList.remove('card--theme3');
+    card.classList.remove('card--theme1');
+
+  } else {
+    card.classList.add('card--theme3');
+    card.classList.remove('card--theme2');
+    card.classList.remove('card--theme1');
+  }
 }
 
-themeTwo.addEventListener('change', paletteTwo);
 
-function paletteTwo() {
-  card.classList.add('card--theme2');
-  card.classList.remove('card--theme3');
-  card.classList.remove('card--theme1');
-}
-
-themeThree.addEventListener('change', paletteThree);
-
-function paletteThree() {
-  card.classList.add('card--theme3');
-  card.classList.remove('card--theme2');
-  card.classList.remove('card--theme1');
-}
+pallete.addEventListener('change', changePallete);
 
 
 // boton reset
