cfe2dc7b9ec968f8338e783e3a306649e3709db9
mj0se <mariajose1907@gmail.com>
2020-03-18 22:07:54 +0100

    changes to objetCardStoreItem

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -11,12 +11,12 @@ const formObject = {
   github: document.querySelector('.js-form-input-github')
 };
 
-formObject.name.addEventListener('keyup', form);
-formObject.job.addEventListener('keyup', form);
-formObject.email.addEventListener('keyup', form);
-formObject.tel.addEventListener('keyup', form);
-formObject.linkedin.addEventListener('keyup', form);
-formObject.github.addEventListener('keyup', form);
+formObject.name.addEventListener('keyup', ensureData);
+formObject.job.addEventListener('keyup', ensureData);
+formObject.email.addEventListener('keyup', ensureData);
+formObject.tel.addEventListener('keyup', ensureData);
+formObject.linkedin.addEventListener('keyup', ensureData);
+formObject.github.addEventListener('keyup', ensureData);
 
 const cardObject = {
   name: document.querySelector('.js-card-title-first'),
@@ -28,31 +28,38 @@ const cardObject = {
   image: document.querySelector('.js-card-img')
 };
 
-function form(event) {
-  if (event.currentTarget.classList.contains('js-form-input-name')) {
-    cardObject.name.innerHTML = event.currentTarget.value;
-  } else if (event.currentTarget.classList.contains('js-form-input-job')) {
-    cardObject.job.innerHTML = event.currentTarget.value;
-  } else if (event.currentTarget.classList.contains('js-form-input-email')) {
-    cardObject.email.setAttribute('href', `mailto: ${formObject.email.value}`);
-  } else if (event.currentTarget.classList.contains('js-form-input-tel')) {
-    cardObject.tel.setAttribute('href', formObject.tel.value);
-  } else if (event.currentTarget.classList.contains('js-form-input-linkedin')) {
-    cardObject.linkedin.setAttribute('href', formObject.linkedin.value);
-  } else if (event.currentTarget.classList.contains('js-form-input-github')) {
-    cardObject.github.setAttribute('href', formObject.github.value);
-  }
-//local storage: 
-/*   for (const i in formObject) {
-    localStorage.setItem('formObject', JSON.stringify(formObject.[i].value));
-  }
-  console.log(cardObject);
 
-  localStorage.setItem('formObject', JSON.stringify(formObject.name.value));
+
+function setLocalStorage() {
+  const data = {};
+  for (const input in formObject) {
+    data[input] = formObject[input].value;
+  }
+  localStorage.setItem('data', JSON.stringify(data));
+  // falta la mandanga de la paleta y la foto
 }
 
-const saveFormObject = JSON.parse(localStorage.getItem('formObject'));
-console.log(saveFormObject); */
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('data'));
+  if (data !== null) {
+    for (const input of formObject) {
+      formObject[input].value = data[input];
+    }
+  }
+  ensureData();
+}
+
+function ensureData() {
+  cardObject.name.innerHTML = formObject.name.value;
+  cardObject.job.innerHTML = formObject.job.value;
+  cardObject.tel.setAttribute('href', formObject.value);
+  cardObject.email.setAttribute('href', formObject.value);
+  cardObject.linkedin.setAttribute('href', formObject.value);
+  cardObject.github.setAttribute('href', formObject.value);
+  setLocalStorage();
+}
+
+getLocalStorage();
 
 // cambiar los colores
 
@@ -67,6 +74,7 @@ function paletteOne() {
   card.classList.add('card--theme1');
   card.classList.remove('card--theme2');
   card.classList.remove('card--theme3');
+  setLocalStorage();
 }
 
 themeTwo.addEventListener('change', paletteTwo);
@@ -75,6 +83,7 @@ function paletteTwo() {
   card.classList.add('card--theme2');
   card.classList.remove('card--theme3');
   card.classList.remove('card--theme1');
+  setLocalStorage();
 }
 
 themeThree.addEventListener('change', paletteThree);
@@ -83,6 +92,7 @@ function paletteThree() {
   card.classList.add('card--theme3');
   card.classList.remove('card--theme2');
   card.classList.remove('card--theme1');
+  setLocalStorage();
 }
 
 // boton reset
@@ -90,7 +100,10 @@ function paletteThree() {
 const btnReset = document.querySelector('.js-btn-reset');
 
 function reset() {
-  document.location.reload();
+  for (const input of formObject) {
+    formObject[input].value = '';
+  }
+  ensureData();
 }
 btnReset.addEventListener('click', reset);
 // Share section Javascript
@@ -124,7 +137,7 @@ function previewFile() {
   var file = document.querySelector('.js-input-invisible').files[0];
   var reader = new FileReader();
 
-  reader.onloadend = function() {
+  reader.onloadend = function () {
     preview.src = reader.result;
     cardObject.image.src = preview.src;
   };
